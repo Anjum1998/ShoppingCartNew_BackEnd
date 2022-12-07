6bf13f059a298057ed52f8f06b1e3b2305e884d8
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-07 18:54:22 +0530

    first commit

---
+++ src/main/java/com/example/ShoppingCart_BackEnd/ShoppingCartBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingCart_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShoppingCart_BackEnd/ShoppingCartBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingCart_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
