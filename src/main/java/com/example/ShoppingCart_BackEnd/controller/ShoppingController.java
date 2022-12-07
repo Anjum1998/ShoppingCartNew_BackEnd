4a164ed7a49740c60558e0010fb70aa7c3b5500b
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-07 23:03:00 +0530

    add product

---
+++ src/main/java/com/example/ShoppingCart_BackEnd/controller/ShoppingController.java
@@ -0,0 +1,25 @@
+package com.example.ShoppingCart_BackEnd.controller;
+
+import com.example.ShoppingCart_BackEnd.dao.ProductDao;
+import com.example.ShoppingCart_BackEnd.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ShoppingController {
+    @Autowired
+    private ProductDao dao;
+
+    @CrossOrigin("*")
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String AddProduct(@RequestBody Product p)
+    {
+        System.out.println(p.getPname().toString());
+        System.out.println(p.getImage().toString());
+        System.out.println(p.getCategory().toString());
+        System.out.println(p.getPrice());
+        System.out.println(p.getDescription().toString());
+        dao.save(p);
+        return "product added";
+    }
+}
---
+++ src/main/java/com/example/ShoppingCart_BackEnd/model/Product.java
@@ -0,0 +1,79 @@
+package com.example.ShoppingCart_BackEnd.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String pname;
+    private String image;
+    private String category;
+    private String description;
+    private int price;
+
+    public Product() {
+    }
+
+    public Product(int id, String pname, String image, String category, String description, int price) {
+        this.id = id;
+        this.pname = pname;
+        this.image = image;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
